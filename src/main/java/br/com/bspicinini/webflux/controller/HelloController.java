f2a2fe0a6f92d42ea37ea4f7b2ad6727548035bc
Bruno de Souza Picinini <brunosouzapicinini@gmail.com>
2022-07-29 00:56:58 -0300

    Hello World

---
+++ src/main/java/br/com/bspicinini/webflux/controller/HelloController.java
@@ -0,0 +1,18 @@
+package br.com.bspicinini.webflux.controller;
+
+import org.reactivestreams.Publisher;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class HelloController {
+
+    @GetMapping
+    @ResponseBody
+    public Publisher<String> sayHello() {
+
+        return Mono.just("Hello World!");
+    }
+}
